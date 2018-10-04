f11ecde06035e27dfd68f77a12750b76683bbcdf
Victor <t5204713910@gmail.com>
2018-10-04 12:25:15 +0800

    trying to make it more flexible
    I add a json file to save the needed information for running this
    module so that we don't need to specify those variables in the main
    script. If the variable is empty in the Preresquite.json, there
    will be an input function to ask the user to enter the needed variables.
    or the user can just add those variables directly into Prerequestite.json

--- calculation.py
+++ calculation.py
@@ -1,6 +1,6 @@
 """
 I think there is no need for specifying csv file name and path. Needs more
-modification.
+modification. => I am working to make a json file to store these information
 
 I also need to check whether the names match csv_files
 
@@ -10,23 +10,62 @@ The next step is to claculate rolling connectedness
 """
 
 # import modules
+import json
 import functions.f_volatility as f_vol
 import functions.f_coef as f_coef
 import functions.f_connectedness as f_conn
 import functions.f_network as f_net
-import pandas as pd
+import os
+import glob
 
+# load Prerequisite
+file_dir = os.path.dirname(os.path.abspath(__file__))
+path = file_dir + '/docs/'
+os.chdir(path)
+with open('Prerequisite.json') as f:
+    data = json.load(f)
+
+# update the Prerequisite
+if data["target_folder"] == {}:
+    print("please speficify the folder filled with csv files to calculate connectedness")
+    data["target_folder"] = input("please input target folder:")
+
+
+"""
+# input variables
+input
+
+# input the target folder name in docs
+target_folder = 'country_stock_csv'
+if target_folder is None:
+    target_folder = input("input target folder:")
+else:
+    pass
+
+
+# obtain csv files from particular folder
+file_dir = os.path.dirname(os.path.abspath(__file__))
+path = file_dir + '/docs/' + target_folder
+os.chdir(path)
+csv_files = glob.glob('**.csv')
+
+
+# setup the names of the variables
+names = input("input the names of the volatility variables:")
+names = ["US", "UK", "Singapore", "HK", "Taiwan", "Japan", "china"]
+if names is None:
+    names = csv_files
+else:
+    pass
 
 # variables for volatility
-names = ["US", "UK", "Singapore", "HK", "Taiwan", "Japan", "china"]
-csv_files = ["^GSPC.csv", "^FTSE.csv", "^STI.csv", "^HSI.csv", "^TWII.csv",
-             "^N225.csv", "000001.SS.csv"]
-path = ("/Users/rucachen/projects/MultiTimeSeries_Connectedness/docs/" +
-        "country_stock_csv")
+start_dt = input("input the start date for volatility dataframe:")
+end_dt = input("input the end data for volatility dataframe:")
 start_dt = "1998-09-01"
 end_dt = "2018-01-01"
+"""
 
-
+"""
 # calculate volatility dataframe
 volatility = f_vol.volatility(names, csv_files, path, start_dt, end_dt)
 volatility.price_data_to_volatility()
@@ -63,3 +102,4 @@ network.change_names(names)
 network.create_network()
 network.plot()
 network.show_draw()
+"""
