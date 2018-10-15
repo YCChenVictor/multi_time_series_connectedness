c083369b07ad37a023b70b4e67e06620cabb076f
Victor <t5204713910@gmail.com>
2018-10-15 17:08:06 +0800

    I modified the __init__ because I don't know how to use inherit appropriately.
    I also finish the divide_dataframe function, which solve the reinput
    dataframe and period problem and the period of divided dataframe
    lower than period specified problem.
    
    I deleted the self.lag because it will recalculate the lag everytime.
    
    I also finish the calculation of rolling connectedness and know try
    to build it as panel data.

--- functions/f_rolling_connectedness.py
+++ functions/f_rolling_connectedness.py
@@ -1,47 +1,101 @@
 """
-After the estimated coefficient calculated, lets build the rolling connectedness
-which is use the coefficient to iteratively calculate connectedness through time.
+I should reveal the period of connectedness I am calculating, not just number.
 
-I got the recalculate the estimated sigma all over the time.
+
 """
 # import the required module
-from functions.f_connectedness import Connectedness
-from functions.f_coef import Coef
+import functions.f_connectedness as f_conn
+import functions.f_coef as f_coef
+import pickle
+import os
+import functions.about_path as f_about_path
 
 
-class Rolling_Connectedness(Coef, Connectedness):
+class Rolling_Connectedness:
 
     def __init__(self, data, max_lag, data_periods):
-        super(Coef, self).__init__(data, max_lag)
+        # to variable to run this module
+        self.data = data
+        self.name = list(data)
+        self.max_lag = max_lag
         self.data_periods = data_periods
-        self.data_list = None
 
-    def divide_dataframe(self, dataframe, periods):
+        # save the calculated connectedness
+        self.data_list = None
+        self.connectedness_list = None
+
+    def divide_dataframe(self):
+
+        # required variables
+        dataframe = self.data
+        periods = self.data_periods
+
         # list to save the data
         data_list = []
 
         # iteratively divide dataframe
         for i in range(len(dataframe)):
-            print(i)
             data = dataframe.iloc[i: periods+i]
+            data = data.reset_index(drop=True)
+            if len(data) < periods:
+                break
             data_list.append(data)
 
         self.data_list = data_list
 
-    def obtain_coefficient(self, lag):
-        self.lag = lag  # calculated from full_connectedness
-
     def calculate_rolling(self):
-        # data_periods = self.data_periods
-        lag = self.lag
-        data = self.data
+
+        data_list = self.data_list
+        max_lag = self.max_lag
 
         # create the list of the rolling dataframe
-        for i in range(1, lag):
-            print(data)
+        connectedness_list = []
+        accuracy_list = []
 
-    def get_specific_rolling_connectedness():
-        pass
+        # index
+        index = 0
+
+        # start to calculate rolling
+        for data in data_list:
+
+            print("calculating rolling, period: %s" % index)
+            # coef and sigma_hat ####
+            coef = f_coef.Coef(data, max_lag)
+            coef.f_ols_coef()
+            ols_coef = coef.OLS_coef
+            ols_sigma = coef.OLS_sigma
+
+            # accuracy
+            accuracy = coef.accuracy
+            accuracy_list.append(accuracy)
+
+            # connectedness
+            conn = f_conn.Connectedness(ols_coef, ols_sigma)
+            conn.f_full_connectedness()
+            connectedness_list.append(conn.full_connectedness)
+            index += 1
+
+        # save rolling connectedness in class
+        self.connectedness_list = connectedness_list
+
+        # save rolling connectedness in pickle
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        file_path = f_about_path.parent_path(current_path, 1)
+        save_path = file_path + '/docs/' + 'roll_conn.pickle'
+        with open(save_path, 'wb') as f:
+            pickle.dump(connectedness_list, f)
+
+    def rolling_to_panel(self):
+
+        # read connectedness in pickle if there is nothing in roll_conn
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        file_path = f_about_path.parent_path(current_path, 2)
+        save_path = file_path + '/docs/' + 'roll_conn.pickle'
+        with open(save_path, 'rb') as f:
+            result = pickle.load(f)
+
+        # restructure it
+        return result
 
     def plot_rolling():
         pass
