f43f91acb1830e5a4fe9401037b78878eea1d972
Victor <t5204713910@gmail.com>
2018-11-03 20:47:24 +0800

    solve the name issue in rolling connectedness and save it as pickle file'

--- roll_conn.py
+++ roll_conn.py
@@ -1,6 +1,5 @@
 import os
 import pickle
-import functions.f_load_data as f_load
 import functions.f_rolling_connectedness as f_roll
 
 # load volatility_dataframe
@@ -9,16 +8,22 @@ save_path = file_path + '/docs/' + 'volatility.pickle'
 with open(save_path, 'rb') as f:
     volatility_dataframe = pickle.load(f)
 
-# obtain the names, csv_files, path, start_dt, end_dt
-names = f_load.names
-csv_files = f_load.csv_files
-path = f_load.path
-start_dt = f_load.start_dt
-end_dt = f_load.end_dt
+# save name of the dataframe
+names = list(volatility_dataframe.columns.values)
+names.remove('Date')
+names.append("all")
 
 # start the rolling connectedness
 roll_conn = (f_roll.
-             Rolling_Connectedness(volatility_dataframe, 20, 80))
+             Rolling_Connectedness(volatility_dataframe, 20, 80, names))
 roll_conn.divide_dataframe()
 roll_conn.calculate_rolling()
-# it will save the calculated dict into roll_conn,pickle
+
+# obtain the rolling connectedness dataframe
+data = roll_conn.rolling_connectedness
+
+# save the volatility_dataframe into pickle
+file_path = os.path.dirname(os.path.realpath(__file__))
+save_path = file_path + '/docs/' + 'roll_conn.pickle'
+with open(save_path, 'wb') as f:
+    pickle.dump(data, f)
