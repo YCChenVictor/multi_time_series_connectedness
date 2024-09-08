6f064c1693569805a49202587df7000f928ebb51
Victor Chen <victor.chen@hoc-trade.com>
2024-09-08 13:25:23 +0800

    feature: extract out volatilities calculation

--- full_connectedness.py
+++ full_connectedness.py
@@ -3,14 +3,11 @@ import functions.f_volatility as f_vol
 import functions.coef as f_coef
 import functions.connectedness as f_conn
 from functions.load_data import get_file_names
+import pandas as pd
 
-def calculate_connectedness(start_at, end_at, files_path):
-    files = get_file_names(files_path)
-
-    # calculate volatility dataframe
-    volatility = f_vol.volatility(files_path, start_at, end_at)
-    volatility.price_data_to_volatility()
-    volatilities = volatility.volatilities
+def calculate_connectedness(volatilities):
+    # get the variable names
+    names = list(volatilities.columns[1:])
     
     # calculate estimated coefficients
     max_lag = 20
@@ -26,20 +23,20 @@ def calculate_connectedness(start_at, end_at, files_path):
     # calculate connectedness
     conn = f_conn.Connectedness(ols_coef, ols_sigma)
     conn.calculate_full_connectedness()
-    conn.rename_table(files + ["all"])
+    conn.rename_table(names + ["all"])
     table = conn.full_connectedness
     return table
     # conn.table_restructure()
 
-def main(start_at, end_at, files_path):
-    connectedness_results = calculate_connectedness(start_at, end_at, files_path)
+def main(path):
+    volatilities = pd.read_pickle(path)
+    connectedness_results = calculate_connectedness(volatilities)
     print("Connectedness Results:", connectedness_results)
 
 if __name__ == "__main__":
+    # specify the path
     parser = argparse.ArgumentParser(description="Process some financial data.")
     parser.add_argument('--path', type=str, help='Path to the data')
-    parser.add_argument('--start_at', type=str, help='Start Time')
-    parser.add_argument('--end_at', type=str, help='End Time')
 
     args = parser.parse_args()
-    main(args.start_at, args.end_at, args.path)
+    main(args.path)
