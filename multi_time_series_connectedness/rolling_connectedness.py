c2e459dc0d87c898f97b20aa819210a59e8f9b3e
Victor Chen <victor.chen@hoc-trade.com>
2024-10-13 15:44:01 +0800

    feature: add timeframe to rolling connectedness calculation

--- multi_time_series_connectedness/rolling_connectedness.py
+++ multi_time_series_connectedness/rolling_connectedness.py
@@ -1,46 +1,41 @@
 from multi_time_series_connectedness.connectedness import Connectedness
 import pandas as pd
 import pickle
+import sys
 
 
 class RollingConnectedness:
-
-    def __init__(self, data, max_lag, data_periods):
-        # to variable to run this module
+    def __init__(self, data, max_lag, data_periods, start_at, end_at):
         self.data = data
         self.max_lag = max_lag
         self.data_periods = data_periods
         self.name = [col for col in data.columns if col != 'time'] + ['all']
+        self.start_at = start_at
+        self.end_at = end_at
 
         # save the calculated connectedness
-        self.data_list = None
+        self.split_data = None
         self.rolling_connectedness = None
         self.accuracy_list = None
 
     def divide_timeseries_volatilities(self):
-        dataframe = self.data
-        periods = self.data_periods
+        data_eariler_than_start_at = self.data[self.data['time'] <= self.start_at]
+        if (len(data_eariler_than_start_at) < self.data_periods):
+            print("There is not enough data for the first connectedness")
+            sys.exit()
 
-        data_list = []
-
-        for i in range(len(dataframe)):
-
-            # get divided data
-            data = dataframe.iloc[i: periods+i]
-            if len(data) < periods:
-                break
-
-            # get the start and end date
-            data = data.reset_index(drop=True)
-
-            # add to data_list
-            data_list.append(data)
-
-        self.data_list = data_list
+        filtered_time = self.data[(self.data['time'] > self.start_at) & (self.data['time'] <= self.end_at)]['time']
+        print(filtered_time)
+        split_data = {}
+        for index, time in filtered_time.items():
+            last_rows = self.data.iloc[index-self.data_periods - 1:index - 1] # I think this line will cause problem
+            split_data[time] = last_rows
+        self.split_data = split_data
 
     def calculate(self, store_result_at, callback_after_one_connectedness=None):
+        self.divide_timeseries_volatilities()
         restructured_connectedness_timeseries = pd.DataFrame()
-        for data in self.data_list:
+        for key, data in self.split_data.items():
             start_date = data["time"].iloc[0]
             end_date = data["time"].iloc[self.data_periods-1]
             period = start_date + " ~ " + end_date
@@ -53,14 +48,17 @@ class RollingConnectedness:
             conn.flatten_connectedness()
 
             restructured_connectedness = conn.restructure_connectedness
+            restructured_connectedness['period'] = key
+            restructured_connectedness.set_index('period', inplace=True)
             if callback_after_one_connectedness:
                 callback_after_one_connectedness(restructured_connectedness)
             restructured_connectedness_timeseries = pd.concat(
                 [restructured_connectedness_timeseries, restructured_connectedness], 
-                ignore_index=True
+                ignore_index=False
             )
 
         self.rolling_connectedness = restructured_connectedness_timeseries
+        print(restructured_connectedness_timeseries)
 
         with open(store_result_at, 'wb') as f:
             pickle.dump(self.rolling_connectedness, f)
